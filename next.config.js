d1c83b309f929ba3ee31389f725ec71db16b9cad
Devemmy <macbookpro@Devemmy.local>
2023-11-21 12:47:20 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,21 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'out',
-  trailingSlash: true,
-  generateEtags: true,
-  distDir: 'out',
+  // output: 'export',
+  // trailingSlash: true,
+  // generateEtags: false,
   // assetPrefix:'https://jamesdroyce.com/experiment/out/_next',
   // basePath: ".",
-  images: {
-    remotePatterns: [ 
-      {
-        protocol: "https",
-        hostname: "www.northstarsurgery.com",
-        port: "",
-        pathname: "/",
-      },
-    ],
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "www.northstarsurgery.com",
+  //       port: "",
+  //       pathname: "/",
+  //     },
+  //   ],
+  // },
 };
 
 module.exports = nextConfig;
